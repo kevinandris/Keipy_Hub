a714da12b73d245f28ff2fbbf7214c37545929c1
kevinandris <kevinandris27@gmail.com>
2024-06-11 08:46:52 +1200

    completed the login and sign-up pages

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+// Preventing the users from accessing the homepage if they are not signed in
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
