b22171aaea49614e173591394f340167613e7228
kevinandris <kevinandris27@gmail.com>
2024-07-10 09:22:49 +1200

    more responsive coded added

--- components/custom/Categories.tsx
+++ components/custom/Categories.tsx
@@ -37,7 +37,7 @@ const Categories = ({ categories, selectedCategory }: CategoriesProps) => {
 
 const CategoryStyle = styled.div`
   /* Width */
-  @media screen and (min-width: 2560px) {
+  @media screen and (max-width: 2560px) {
     margin-top: 2rem;
   }
 
@@ -45,60 +45,160 @@ const CategoryStyle = styled.div`
     margin-top: 5rem;
   }
 
+  @media screen and (max-width: 1681px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1621px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1619px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1611px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1609px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1603px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1599px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1597px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1595px) {
+    margin-top: 9.5rem;
+  }
+
+  @media screen and (max-width: 1593px) {
+    margin-top: 10rem;
+  }
+
+  @media screen and (max-width: 1583px) {
+    margin-top: 12rem;
+  }
+
+  @media screen and (max-width: 1581px) {
+    margin-top: 12rem;
+  }
+
   @media screen and (max-width: 1565px) {
-    margin-top: 5rem;
+    margin-top: 13rem;
   }
 
   @media screen and (max-width: 1354px) {
-    margin-top: 7rem;
+    margin-top: 13rem;
   }
 
   @media screen and (max-width: 1230px) {
-    margin-top: 8rem;
+    margin-top: 12rem;
   }
 
-  @media screen and (max-width: 812px) {
+  @media screen and (max-width: 859px) {
+    margin-top: 16rem;
+  }
+
+  @media screen and (max-width: 810px) {
+    margin-top: 13rem;
+  }
+
+  @media screen and (max-width: 800px) {
     margin-top: 11rem;
   }
 
   @media screen and (max-width: 788px) {
-    margin-top: 8rem;
-  }
-
-  @media screen and (max-width: 768px) {
-    margin-top: 12rem;
-  }
-
-  @media screen and (max-width: 750px) {
-    margin-top: 12rem;
-  }
-
-  @media screen and (max-width: 664px) {
-    margin-top: 9rem;
-  }
-
-  @media screen and (max-width: 650px) {
-    margin-top: 13rem;
-  }
-
-  @media screen and (max-width: 506px) {
     margin-top: 16rem;
   }
 
-  @media screen and (max-width: 428px) {
+  @media screen and (max-width: 768px) {
+    margin-top: 16rem;
+  }
+
+  @media screen and (max-width: 750px) {
+    margin-top: 16rem;
+  }
+
+  @media screen and (max-width: 670px) {
+    margin-top: 16rem;
+  }
+
+  @media screen and (max-width: 655px) {
+    margin-top: 19rem;
+  }
+
+  @media screen and (max-width: 513px) {
     margin-top: 22rem;
   }
 
-  @media screen and (max-width: 378px) {
+  @media screen and (max-width: 435px) {
     margin-top: 26rem;
   }
 
-  @media screen and (max-width: 342px) {
-    margin-top: 26rem;
+  @media screen and (max-width: 428px) {
+    margin-top: 24rem;
   }
 
-  @media screen and (max-width: 334px) {
+  @media screen and (max-width: 414px) {
+    margin-top: 24rem;
+  }
+
+  @media screen and (max-width: 381px) {
     margin-top: 29rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    margin-top: 28rem;
+  }
+
+  @media screen and (max-width: 360px) {
+    margin-top: 28rem;
+  }
+
+  @media screen and (max-width: 345px) {
+    margin-top: 32rem;
+  }
+
+  @media screen and (max-width: 335px) {
+    margin-top: 35rem;
+  }
+
+  @media screen and (max-width: 309px) {
+    margin-top: 38rem;
+  }
+
+  @media screen and (max-width: 303px) {
+    margin-top: 41rem;
+  }
+
+  @media screen and (max-width: 265px) {
+    margin-top: 45rem;
+  }
+
+  @media screen and (max-width: 249px) {
+    margin-top: 48rem;
+  }
+
+  @media screen and (max-width: 227px) {
+    margin-top: 51rem;
+  }
+
+  @media screen and (max-width: 215px) {
+    margin-top: 54rem;
+  }
+
+  @media screen and (max-width: 213px) {
+    margin-top: 57rem;
   }
 
   /* Height */
@@ -110,11 +210,11 @@ const CategoryStyle = styled.div`
     margin-top: 10rem;
   } */
 
-  @media screen and (max-height: 760px) {
-    margin-top: 25rem;
-  }
+  /* @media screen and (min-height: 760px) {
+    margin-top: 22rem;
+  } */
 
-  @media screen and (max-height: 730px) {
+  /* @media screen and (max-height: 730px) {
     margin-top: 25rem;
   }
 
@@ -148,7 +248,7 @@ const CategoryStyle = styled.div`
 
   @media screen and (max-height: 629px) {
     margin-top: 25rem;
-  }
+  } */
 `;
 
 export default Categories;
